22375ff8e3d834fa05728a0fed60d6f6e2205473
jereme <jeremejazz15@gmail.com>
2019-09-13 15:36:53 +0800

    add prefix lookup and xtremeldr files

---
+++ scraping/cellprefix.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+
+"""
+Lookup for phone number prefixes. Also creates a local copy for offline use 
+"""
+
+
+if __name__ == '__main__':
+	print("Hello, World!")
---
+++ scraping/xtremeloader.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python3
+
+"""
+Xtremeloader reloaded... 
+
+Lookup for lx products 
+
+
+sample curl calls: 
+
+curl -d 'state=productlist&substate=categorized&category=Cellphone+EPINs' -X POST 'https://loadxtreme.ph/cgi-bin/ajax.cgi'
+
+
+(refer to xtremeloader repo)
+
+
+List of cat:
+[Cellphone EPINs]
+[Internet / Broadband] -  Internet / Broadband </option>
+[Landline] -  Landline </option>
+[Online Entertainment] -  Online Entertainment </option>
+[Online Games] -  Online Games </option>
+[Over-The-Air (OTA)] -  Over-The-Air (OTA) </option>
+[Satellite / Cable TV] -  Satellite / Cable TV </option>
+[Utilities] -  Utilities </option>
+[myLIFE] -  myLIFE </option>
+
+
+"""
+
+if __name__ == '__main__':
+	print("Hello, World!")
