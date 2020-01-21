09334d5bec2d796f53edab48a77c13ac9fccb199
jereme <jeremejazz15@gmail.com>
2020-01-21 11:46:42 +0800

    simplify computing mid

--- algorithms/binary.py
+++ algorithms/binary.py
@@ -4,16 +4,15 @@ import math
 def binarysearch(search, sortedlist):
     left = 0
     right = len(sortedlist) -1
-    mid = math.ceil(right / 2)
+    mid = math.ceil((right + left) / 2)
     
     while sortedlist[mid] != search:
         if search > sortedlist[mid]:
             left = mid+1
         else:
             right = mid-1
-        mid = left + math.ceil((right - left)/2)
-    print(mid)
-    
+        mid = math.ceil((right + left)/2)
+    return mid 
 
-arr = [1,3,44,55,66,78,109]
-binarysearch(78,arr)
+arr = [1,3,4,5,44,55,66,78,109,1000]
+print(binarysearch(3,arr))
