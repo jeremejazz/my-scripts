1b64fb3b052c2b9272e82a9b1b795c1980dc35d0
jereme <jeremejazz15@gmail.com>
2020-01-23 15:46:54 +0800

    add if not found

--- algorithms/binary.py
+++ algorithms/binary.py
@@ -11,8 +11,12 @@ def binarysearch(search, sortedlist):
             left = mid+1
         else:
             right = mid-1
+        if left > right or right < left:
+            return -1
         mid = math.ceil((right + left)/2)
+
     return mid 
 
 arr = [1,3,4,5,44,55,66,78,109,1000]
-print(binarysearch(3,arr))
+print(arr)
+print(binarysearch( int(input("Enter num: ")),arr))
